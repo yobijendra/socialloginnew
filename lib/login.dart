58bb19b3c0836ea92c8055497c7ae5c98c4b4dde
singhBijendraIOS <bsbhatia@celestiaslsys.com>
2019-06-11 19:26:03 +0530

    First

--- lib/login.dart
+++ lib/login.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_auth_buttons/flutter_auth_buttons.dart';
-//import 'package:flutter_facebook_login/flutter_facebook_login.dart';
+import 'package:flutter_facebook_login/flutter_facebook_login.dart';
 
 class login extends StatefulWidget {
   @override
@@ -10,14 +10,33 @@ class login extends StatefulWidget {
 
 class _loginState extends State<login> {
 
+  FirebaseAuth _auth = FirebaseAuth.instance;
+  bool isLogged = false;
+
+  Future<FirebaseUser> _loginwithFacebook ()
+  async {
+    var facebookLogin = new FacebookLogin();
+    var facebookLoginResult = await facebookLogin.logInWithReadPermissions(['email']);
+    debugPrint( 'return data is $facebookLoginResult.accessToken.toString()');
+    print('Data >>>>>> is $facebookLoginResult');
+    switch (facebookLoginResult.status) {
+      case FacebookLoginStatus.error:
+        print("Error");
+        break;
+      case FacebookLoginStatus.cancelledByUser:
+        print("CancelledByUser");
+        break;
+      case FacebookLoginStatus.loggedIn:
+        print("LoggedIn");
+        break;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(appBar: AppBar(title: Text('Login')),
         body: Center(child: Column(children: <Widget>[
-          GoogleSignInButton(onPressed: () {}),
-          GoogleSignInButton(onPressed: () {}, darkMode: true),
-          FacebookSignInButton(onPressed: () {}),
-          TwitterSignInButton(onPressed: () {}),
+          FacebookSignInButton(onPressed:_loginwithFacebook),
     ],
         ),
         )
